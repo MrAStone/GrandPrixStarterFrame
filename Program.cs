0bc5c5f155e9c0d0b5777f3b69dab69f6ffbe59a
Al Stone <astone@heckgrammar.co.uk>
2025-04-01 15:13:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Device.Gpio;
+namespace GrandPrixStarterFrame
+{
+    internal class Program
+    {
+        static bool done;
+        static void Main(string[] args)
+        {
+            //pi5
+            GpioController c = new();
+            //pi4
+            // GpioController c = new GpioController(PinNumberingScheme.Board);
+            c.OpenPin(21, PinMode.InputPullUp);
+            done = false;
+            while (!done)
+            {
+                c.RegisterCallbackForPinValueChangedEvent(21, PinEventTypes.Falling, OnPinEvent);
+            }
+        }
+        static void OnPinEvent(object sender, PinValueChangedEventArgs pinArgs)
+        {
+            startSequence();
+        }
+        static void startSequence()
+        {
+            //Write the code to light up 5 LEDs in sequence
+            //and then turn them off
+
+
+            done = true;
+        }
+    }
+}
